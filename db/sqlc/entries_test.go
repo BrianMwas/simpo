323e4ae9d23914469d0021ff527167d1b21600de
Brian Mwangi <42889128+BrianMwas@users.noreply.github.com>
2022-09-02 15:08:50 +0300

    initial commit

---
+++ db/sqlc/entries_test.go
@@ -0,0 +1,18 @@
+package db
+
+import "testing"
+
+// TestCreateEntry: Test creating an entry
+func TestCreateEntry(t *testing.T) {
+
+}
+
+// TestGetEntry: Test getting an entry from an entry id
+func TestGetEntry(t *testing.T) {
+
+}
+
+// TestListEntries: Test getting a list of entries
+func TestListEntries(t *testing.T) {
+
+}

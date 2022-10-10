7c4596e0f93b4e9b0a5aec2bf2c5313e11a353e9
Brian Mwangi <42889128+BrianMwas@users.noreply.github.com>
2022-10-10 11:38:18 +0300

    Set up configurations for api keys

---
+++ util/config.go
@@ -0,0 +1,26 @@
+package util
+
+import "github.com/spf13/viper"
+
+// Config Store application configurations
+type Config struct {
+	DBDriver      string `mapstructure:"DB_DRIVER"`
+	DBSource      string `mapstructure:"DB_SOURCE"`
+	ServerAddress string `mapstructure:"SERVER_ADDRESS"`
+}
+
+// LoadConfig loads up configurations from a file or environment variables if it exists
+func LoadConfig(path string) (config Config, err error) {
+	viper.AddConfigPath(path)
+	viper.SetConfigName("app")
+	viper.SetConfigType("env")
+
+	viper.AutomaticEnv()
+	err = viper.ReadInConfig()
+	if err != nil {
+		return
+	}
+
+	err = viper.Unmarshal(&config)
+	return
+}

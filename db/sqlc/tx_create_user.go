a641fa0280745c2176d5a2da9fa78db14471b766
BrianMwas <brnmwas@gmail.com>
2023-01-12 10:36:52 +0300

    Updated comments to be more meaningful

--- db/sqlc/tx_create_user.go
+++ db/sqlc/tx_create_user.go
@@ -2,18 +2,19 @@ package db
 
 import "context"
 
-// CreateUserTxParams
+// CreateUserTxParams transaction request body with a callback which will be returned and used to handle
+// an action only when the CreateUserTX passes
 type CreateUserTxParams struct {
 	CreateUserParams
 	AfterCreate func(user User) error
 }
 
-// CreateUserTxResult
+// CreateUserTxResult a transaction response to return a user.
 type CreateUserTxResult struct {
 	User
 }
 
-// CreateUserTX
+// CreateUserTX creates a user with a rollback if the process fails
 func (store *SQLStore) CreateUserTX(ctx context.Context, arg CreateUserTxParams) (CreateUserTxResult, error) {
 	var result CreateUserTxResult
 	err := store.execTx(ctx, func(q *Queries) error {

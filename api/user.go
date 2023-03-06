3417d7964b0a433f1d69ecb02b3da5a52853bcf7
BrianMwas <brnmwas@gmail.com>
2023-03-06 12:38:00 +0300

    Update models for verify emails db model

--- api/user.go
+++ api/user.go
@@ -16,13 +16,13 @@ type createUserRequest struct {
 	Username string `json:"username" binding:"required,alphanum"`
 	Password string `json:"password" binding:"required,min=6"`
 	FullName string `json:"full_name" binding:"required"`
-	Email    string `json:"email" binding:"required,email"`
+	Email    string `json:"mail" binding:"required,mail"`
 }
 
 type userResponse struct {
 	Username          string    `json:"username"`
 	FullName          string    `json:"full_name"`
-	Email             string    `json:"email"`
+	Email             string    `json:"mail"`
 	PasswordChangedAt time.Time `json:"password_changed_at"`
 	CreatedAt         time.Time `json:"created_at"`
 }

ae9582e8edf32df1bd0f3a98ef45ddddfd14b903
BrianMwas <brnmwas@gmail.com>
2023-03-06 12:36:47 +0300

    Added email account for sending emails

--- util/config.go
+++ util/config.go
@@ -18,6 +18,9 @@ type Config struct {
 	AccessTokenDuration  time.Duration `mapstructure:"ACCESS_TOKEN_DURATION"`
 	RefreshTokenDuration time.Duration `mapstructure:"REFRESH_TOKEN_DURATION"`
 	RedisAddress         string        `mapstructure:"REDIS_ADDRESS"`
+	EmailSenderName      string        `mapstructure:"EMAIL_SENDER_NAME"`
+	EmailSenderPassword  string        `mapstructure:"EMAIL_SENDER_PASSWORD"`
+	EmailSenderAddress   string        `mapstructure:"EMAIL_SENDER_ADDRESS"`
 }
 
 // LoadConfig loads up configurations from a file or environment variables if it exists
